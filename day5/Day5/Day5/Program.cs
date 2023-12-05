d5594b9ba94a029b2ffa2ffea9678c6e6536e996
kenan238 <kenanyazbeck@gmail.com>
2023-12-05 14:40:28 +0200

    Unfinished part 2

---
+++ day5/Day5/Day5/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+
+namespace Day5
+{
+  public class Program
+  {
+    private static (ulong[], SeedRange[]) ParseSeeds(StreamReader reader, string? line)
+    {
+      line = line.Replace("seeds: ", "");
+      string[] splitted = line.Split(' ');
+      ulong[] numbers = splitted.Select(x => Convert.ToUInt64(x)).ToArray();
+      List<SeedRange> seeds = new();
+      for (int i = 0; i < numbers.Length; i += 2)
+      {
+        ulong current = numbers[i];
+        ulong future = numbers[i + 1];
+        seeds.Add(new SeedRange {
+          Start = current,
+          Length = future
+        });
+      }
+      return (numbers, seeds.ToArray());
+    }
+
+    private static (string[], string) ParseMap(StreamReader reader, string? line)
+    {
+      string[] title = line.Split(' ');
+      List<string> matrix = new();
+      string name = title[0];
+
+      while ((line = reader.ReadLine()) != null)
+      {
+        if (line == "" || line == "\n")
+          break;
+        matrix.Add(line);
+      }
+
+      return (matrix.ToArray(), name);
+    }
+
+    private static ulong[] seeds;
+    private static SeedRange[] seedsAsRanges;
+    private static Dictionary<string, Range.Type> titleToType = new Dictionary<string, Range.Type>
+    {
+      {"seed-to-soil", Range.Type.SEED_TO_SOIL},
+      {"soil-to-fertilizer", Range.Type.SOIL_TO_FERTILIZER},
+      {"fertilizer-to-water", Range.Type.FERTILIZER_TO_WATER},
+      {"water-to-light", Range.Type.WATER_TO_LIGHT},
+      {"light-to-temperature", Range.Type.LIGHT_TO_TEMP},
+      {"temperature-to-humidity", Range.Type.TEMP_TO_HUMIDITY},
+      {"humidity-to-location", Range.Type.HUMID_TO_LOCATION},
+    };
+    private static Dictionary<Range.Type, List<Range>> conversionMaps = new();
+
+    public static void Main()
+    {
+      StreamReader data = File.OpenText("./data.txt");
+      string? line;
+      while ((line = data.ReadLine()) != null)
+      {
+        if (line.StartsWith("seeds: "))
+        {
+          var returned = ParseSeeds(data, line);
+          seeds = returned.Item1;
+          seedsAsRanges = returned.Item2;
+        }
+        else if (line.Contains("map"))
+        {
+          (string[], string) returned = ParseMap(data, line);
+          string[] content = returned.Item1;
+          Range.Type type = titleToType[returned.Item2];
+
+          conversionMaps.Add(type, new());
+
+          foreach (var range in content)
+            conversionMaps[type].Add(new(range));
+        }
+      }
+
+      // Part 1
+      List<ulong> locations = new();
+
+      foreach (ulong initial in seeds)
+      {
+        ulong seed = initial;
+        Range.Type targetType = Range.Type.SEED_TO_SOIL;
+        while (targetType <= Range.Type.HUMID_TO_LOCATION)
+        {
+          Range adequate = Range.FindAdequate(conversionMaps[targetType], seed);
+          if (adequate is null)
+          {
+            targetType = (Range.Type)((int)targetType + 1);
+            continue; // doesnt change
+          }
+
+          ulong converted = adequate.ConvertSource(seed)!.Value;
+          seed = converted;
+          targetType = (Range.Type)((int)targetType + 1);
+        }
+
+        locations.Add(seed);
+      }
+
+      Console.WriteLine("Part 1: " + locations.Min());
+
+      // Part 2
+      locations = new();
+
+      var counter = 0;
+
+      foreach (SeedRange initial in seedsAsRanges)
+      {
+        //Console.WriteLine("=====================");
+        Range adeq = Range.FindAdequate(conversionMaps[Range.Type.SEED_TO_SOIL], initial);
+        ulong seed = (ulong)adeq.ConvertSource(initial);
+        Range.Type targetType = Range.Type.SEED_TO_SOIL;
+
+        //Console.WriteLine("part2 seed = " + seed.ToString());
+        while (targetType <= Range.Type.HUMID_TO_LOCATION)
+        {
+          Range adequate = Range.FindAdequate(conversionMaps[targetType], seed);
+          if (adequate is null)
+          {
+            targetType = (Range.Type)((int)targetType + 1);
+            //Console.WriteLine(targetType.ToString() + " `= " + seed);
+            continue; // doesnt change
+          }
+
+          ulong converted = adequate.ConvertSource(seed)!.Value;
+          seed = converted;
+          //Console.WriteLine(targetType.ToString() + " = " + seed);
+          targetType = (Range.Type)((int)targetType + 1);
+        }
+
+        locations.Add(seed);
+        Console.WriteLine(counter++);
+        Console.WriteLine(seedsAsRanges.Count());
+      }
+
+      Console.WriteLine("Part 2: " + locations.Min());
+    }
+  }
+}
---
+++ day5/Day5/Day5/Range.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+  public struct SeedRange
+  {
+    public ulong Start;
+    public ulong Length;
+
+    public void FindOverlap(Range r)
+    {
+      if (r == null)
+        return;
+
+      // Gotta code this
+    }
+  }
+
+  public class Range
+  {
+    public enum Type
+    {
+      SEED_TO_SOIL,
+      SOIL_TO_FERTILIZER,
+      FERTILIZER_TO_WATER,
+      WATER_TO_LIGHT,
+      LIGHT_TO_TEMP,
+      TEMP_TO_HUMIDITY,
+      HUMID_TO_LOCATION
+    }
+
+    public ulong DestinationStart { get; set; }
+    public ulong SourceRangeStart { get; set; }
+    public ulong Length { get; set; }
+
+    public Range(string toParse)
+    {
+      string[] split = toParse.Split(' ');
+      DestinationStart = Convert.ToUInt64(split[0]);
+      SourceRangeStart = Convert.ToUInt64(split[1]);
+      Length = Convert.ToUInt64(split[2]);
+    }
+
+    public ulong? ConvertSource(ulong source)
+    {
+      // calculate difference between destination start and source range start
+      ulong diff = SourceRangeStart - DestinationStart;
+      ulong converted = source - diff;
+      bool withinRange = DestinationStart <= converted && (DestinationStart + Length) >= converted;
+      if (!withinRange)
+        return null; // can't convert
+      return converted;
+    }
+
+    public ulong? ConvertSource(SeedRange source)
+    {
+      for (ulong i = source.Start + 1; i < source.Start + Length; i++)
+      {
+        ulong? conv = ConvertSource(i);
+        if (conv != null)
+          return conv;
+      }
+
+      return null;
+    }
+
+    public static Range FindAdequate(List<Range> ranges, ulong nbr)
+    {
+      Range? adequate = null;
+
+      foreach (Range range in ranges)
+      {
+        ulong? conversion = range.ConvertSource(nbr);
+        if (conversion is not null)
+        {
+          adequate = range;
+          break;
+        }
+      }
+
+      return adequate;
+    }
+
+    public static Range FindAdequate(List<Range> ranges, SeedRange nbr)
+    {
+      Range? adequate = null;
+
+      foreach (Range range in ranges)
+      {
+        ulong? conversion = range.ConvertSource(nbr);
+        if (conversion is not null)
+        {
+          adequate = range;
+          break;
+        }
+      }
+
+      return adequate;
+    }
+  }
+}
